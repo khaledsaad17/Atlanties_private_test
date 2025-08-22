7bc929cd4e0d745ca6f9f36a5a3719f59c62d356
khaledsaad17 <98756603+khaledsaad17@users.noreply.github.com>
2025-08-22 13:32:12 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const  express = require('express');
+const mongoose = require('mongoose');
+const login_route = require('./routers/login');
+const register_route = require('./routers/register');
+const Auth = require('./middleware/Auth');
+
+const app = express()
+
+
+
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+
+// database connection
+const local_db_url = process.env.local_db_url
+console.log(local_db_url)
+try {
+    mongoose.connect(local_db_url).then(console.log("database is running"))
+} catch (err) {
+    console.log("error in database connection \n"+err)
+}
+
+
+// server listen
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+// login route
+app.use('/login',login_route)
+
+
+// register route
+
+app.use('/register',register_route)
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/protected',Auth,(req,res)=>{
+    console.log("done you are authorized to login ...........")
+    res.status(201).json({user:req.user})
+})
